2d5d5daee6f3c979b4d322ba5fe5a0e6005799dc
zotlabs <mike@macgirvin.com>
2017-10-29 20:29:45 -0700

    enforce https

--- Mastofeed/Mastofeed.php
+++ Mastofeed/Mastofeed.php
@@ -8,15 +8,16 @@ namespace Zotlabs\Widget;
  * args:
  *   'url' (required)
  *       fully qualified URL to your mastodon homepage, e.g. 'https://foobar.com/users/yourname'
- *
+ *       restriction: must be an https:// URL
  */
 
 
 class Mastofeed {
 
 	function widget($args) {
-		if(! $args['url'])
+		if((! $args['url']) || (substr($args['url'],0,8) !== 'https://'))
 			return '';
+		
 
 		$o = '<iframe allowfullscreen sandbox="allow-top-navigation allow-scripts" width="270" height="9000" frameBorder="0" scrolling="no" src="https://www.mastofeed.com/api/feed?url=' . urlencode($args['url']) . '.atom&theme=light&size=80&header=false"></iframe>';
 
